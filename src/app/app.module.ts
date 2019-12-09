53807ebcaff25254cf1bf82675adc85bfa2a9ea8
aad13 <adam.lounes@yahoo.fr>
2019-12-09 05:50:29 +0100

    Filled app.module with lastly added elements and related lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {TripService} from './service/trip.service';
+import {UserService} from './service/user.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [
+    TripService,
+    UserService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
